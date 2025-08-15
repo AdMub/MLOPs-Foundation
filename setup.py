86dd3ea518ea175a11ada1c81942404d6b8fc310
Adisa Mubarak (AdMub) <99817240+AdMub@users.noreply.github.com>
2025-08-15 11:27:01 +0100

    changes

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
+from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()  # renamed variable for clarity
+    long_description = f.read()     
+   
 
-__version__ = "0.0.5"
-REPO_NAME = "MLOPs-Foundation"
-PKG_NAME = "mongodb_automate"
+__version__ = "0.0.6"
+REPO_NAME = "MLOPS-FOUNDATION"
+PKG_NAME= "mongodbautomate"
 AUTHOR_USER_NAME = "AdMub"
 AUTHOR_EMAIL = "admub465@gmail.com"
 
@@ -14,14 +16,13 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A Python package for connecting with MongoDB.",
-    long_description=long_description,  
-    long_description_content_type="text/markdown", 
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",  
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.7",
-)
+    )
